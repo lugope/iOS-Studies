4ca6167bd2beb6acc0109f2b31821045a6843127
lugope <lucasgpereira.10@gmail.com>
2021-07-21 00:03:45 -0300

    Creating PageView and declaring PageViewController as a child view.

---
+++ SwiftUI-UIKit-tut/SwiftUI-UIKit-tut/Views/PageView/PageView.swift
@@ -0,0 +1,27 @@
+//
+//  PageView.swift
+//  SwiftUI-UIKit-tut
+//
+//  Created by Lucas on 20/07/21.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    
+    var body: some View {
+        PageViewController(pages: pages)
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView(
+            pages: ModelData().features.map {
+                FeatureCard(landmark: $0)
+            }
+        )
+        .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
