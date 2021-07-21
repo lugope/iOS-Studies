6c6e4555b913428349c8bbbf4f5454378a94cde1
lugope <lucasgpereira.10@gmail.com>
2021-07-20 23:40:18 -0300

    Organizing project structure and defining project Model acourding to the resources

---
+++ SwiftUI-UIKit-tut/SwiftUI-UIKit-tut/Views/PageView/FeatureCard.swift
@@ -0,0 +1,24 @@
+//
+//  FeatureCard.swift
+//  SwiftUI-UIKit-tut
+//
+//  Created by Lucas on 20/07/21.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        landmark.featureImage?
+            .resizable()
+            .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
+
+struct FeatureCard_Previews: PreviewProvider {
+    static var previews: some View {
+        FeatureCard(landmark: ModelData().features[0])
+    }
+}
