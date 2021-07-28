cb3b9868e78c128e4cec888cf1e2c1a7c657bb4e
lugope <lucasgpereira.10@gmail.com>
2021-07-28 14:23:57 -0300

    Compleating chapter 3 about collections

--- Swift5_Essentials_Training.playground/Pages/Ch3_WorkingWithCollections.xcplaygroundpage/Contents.swift
+++ Swift5_Essentials_Training.playground/Pages/Ch3_WorkingWithCollections.xcplaygroundpage/Contents.swift
@@ -34,4 +34,73 @@ var attackTreeSkill = skillTree[0][2]
 
 
 // MARK: Swift Dictionaries
+var blacksmithShop: [String: Int] = ["Bottle": 10, "Shield": 15, "Ocarina": 1000]
+//Accessing and modifying values
+var shieldPrice = blacksmithShop["Shield"]
+blacksmithShop["Bottle"] = 5
+blacksmithShop["Towel"] = 42 //If the key doesn't exist swift will add the new element automatically
+//All keys and values
+var allKeys = [String](blacksmithShop.keys)
+var allValues = [Int](blacksmithShop.values)
 
+
+//MARK: Core dictionary methods
+//Caching and overwriting items
+var playerStats: [String: Int] = ["HP":100, "Attack":35, "Gold":29]
+var oldValue = playerStats.updateValue(30, forKey: "Attack")
+//Caching and removing
+var removedValue = playerStats.removeValue(forKey: "Gold")
+//Nested dictionaries
+var questBoard = [
+    "Fetch Gemstone": [
+        "Objective": "Retrieve 4 gemstones",
+        "Challenge": "Complete under 5 minutes"
+    ],
+    "Defeat the boss": [
+        "Objective": "Beat the Boss",
+        "Challenge": "Don't get hit"
+    ]
+]
+var gemstoneObjective = questBoard["Fetch Gemstone"]?["Objective"]
+
+
+//MARK: Working with Sets
+///Setts are great tools if you need to store unique , unordered values in a collection, as opposed to arrays and dictionaries which can hold duplicates
+var setOfNames: Set = ["Jorge", "Pedro", "Lucas"]
+//Inserting and removing elements
+setOfNames.insert("Bia")
+setOfNames.remove("Lucas")
+//More common methods
+setOfNames.contains("Wanderson")
+setOfNames.sorted()
+
+
+//MARK: Set core methods
+var activeQuests: Set = ["Fetch Gemstones", "Big Boss", "The Undertaker"]
+var completedQuests: Set = ["Big Boss", "All-4-One", "The Hereafter"]
+//Set operations
+var commomQuests = activeQuests.intersection(completedQuests)
+var differentQuests = activeQuests.symmetricDifference(completedQuests)
+var allQuests = activeQuests.union(completedQuests)
+var clippedQuests = activeQuests.subtracting(completedQuests)
+
+
+//MARK: Swift Tuples
+///Tuples have the hability to store values of different types
+//Simple tuple
+var uppercutAttack: (String, Int, Bool) = ("Uppercut Smash", 25, true)
+uppercutAttack.0
+uppercutAttack.1
+uppercutAttack.2
+var (attack, damage, rechargeable) = uppercutAttack
+attack
+damage
+rechargeable
+//Naming tuple values
+var planetSmash = (name: "Planet Smash", damage: 100, rechargeable: false)
+planetSmash.name
+planetSmash.damage
+planetSmash.rechargeable
+//Naming with type notation
+var shieldStomp: (name: String, damage: Int, rechargeable: Bool)
+shieldStomp.damage = 100
