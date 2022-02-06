e21cfb4a9b1cd200483b1fc98546ba13f160cdc4
Lucas Pereira <lucasgpereira.10@gmail.com>
2022-02-07 00:47:46 +0100

    Starting project with initial landscape screen configs

---
+++ FlyingBubble/FlyingBubble/GameScene.swift
@@ -0,0 +1,29 @@
+//
+//  GameScene.swift
+//  FlyingBubble
+//
+//  Created by Lucas Pereira on 04/02/22.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        //Method, which is called when your game scene is ready to run.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //Method, which is called when the user touches the screen.
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //Method, which is called when the user stops touching the screen.
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+        
+    }
+}
