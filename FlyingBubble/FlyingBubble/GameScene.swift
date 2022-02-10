62a851b3ef77f194a282d0783806ccc4049628b0
Lucas Pereira <lucasgpereira.10@gmail.com>
2022-02-10 14:42:20 +0100

    Adding backgound with parallax

--- FlyingBubble/FlyingBubble/GameScene.swift
+++ FlyingBubble/FlyingBubble/GameScene.swift
@@ -21,14 +21,43 @@ class GameScene: SKScene {
         player.physicsBody = SKPhysicsBody(circleOfRadius: player.frame.width/2)
 //        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.texture!.size())
         
+        //Adding background
+        parallaxScroll(image: "background3", y: 0, z: -3, duration: 18, needsPhysics: false)
+        parallaxScroll(image: "background2", y: 0, z: -2, duration: 8, needsPhysics: false)
+        parallaxScroll(image: "background1", y: 0, z: -1, duration: 4, needsPhysics: false)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         //Method, which is called when the user touches the screen.
+        player.physicsBody?.velocity = CGVector(dx: 0, dy: 300)
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         //Method, which is called when the user stops touching the screen.
+    }
+    
+    // Makes parallax happens
+    func parallaxScroll(image: String, y: CGFloat, z: CGFloat, duration: Double, needsPhysics: Bool) {
+        for i in 0 ... 1 {
+            let node = SKSpriteNode(imageNamed: image)
+            
+            //position first node on the left and the secound on the right
+            node.position = CGPoint(x: 1023 * CGFloat(i), y: y)
+            node.zPosition = z
+            addChild(node)
+            
+            //make this node move the width of the screen by a duration
+            let move = SKAction.moveBy(x: -1024, y: 0, duration: duration)
+            //make it jump back to the right edge
+            let wrap = SKAction.moveBy(x: 1024, y: 0, duration: 0)
+            
+            //make these two as a sequence that loops forever
+            let sequence = SKAction.sequence([move, wrap])
+            let forever = SKAction.repeatForever(sequence)
+            
+            //run animations
+            node.run(forever)
+        }
     }
     
     override func update(_ currentTime: TimeInterval) {
