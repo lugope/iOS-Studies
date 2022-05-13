939519849388f6f7e7c8d2cdebc3ec6ab555b786
Lucas Pereira <lucasgpereira.10@gmail.com>
2022-05-13 17:41:15 +0200

    CoreDataTut: Restructuring project to add service

--- CoreDataTut/CoreDataTut/Services/BookwormDataService.swift
+++ CoreDataTut/CoreDataTut/Services/BookwormDataService.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import CoreData
+
+class BookwormDataService {
+    private let container: NSPersistentContainer
+    private let containerName: String = "Bookworm"
+    private let entityName: String = "Course"
+    
+    @Published var courseEntities: [Course] = []
+    
+    init() {
+        container = NSPersistentContainer(name: containerName)
+        container.loadPersistentStores { (_, error) in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+            
+        }
+    }
+}
